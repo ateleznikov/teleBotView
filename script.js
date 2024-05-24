3001def803e4fb9db8cd560c6303d1bec1c60797
ateleznikov <124740915+ateleznikov@users.noreply.github.com>
2024-05-24 15:09:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-// Extract the user_id from the URL query parameter
+// Extract the tg_id from the URL query parameter
 const urlParams = new URLSearchParams(window.location.search);
-const userId = urlParams.get('user_id');
+const tgId = urlParams.get('tg_id');
 
 // Function to fetch balance and spent data from the API
 async function fetchBalanceData() {
     try {
-        const response = await fetch(`http://localhost:8000/api/balance?user_id=${userId}`);
+        const response = await fetch(`http://localhost:8000/api/balance?tg_id=${tgId}`);
         const data = await response.json();
         
         if (data.error) {
@@ -20,6 +20,3 @@ async function fetchBalanceData() {
         console.error('Error:', error);
     }
 }
-
-// Call the fetchBalanceData function when the page loads
-window.addEventListener('load', fetchBalanceData);
